84dc457fd28b6d1c2e923638d48c630a1ad10a98
choco-luo <chocoyuru0081@gmail.com>
2025-10-22 13:23:32 +0800

    modify: youbike.js feature

--- 1022/youbike.js
+++ 1022/youbike.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("loadBtn")
 const output = document.getElementById("output")
+const searchBox = document.querySelector(".search-box")
 
 btn.addEventListener("click", () => {
     const url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
@@ -8,7 +9,16 @@ fetch(url)
     .then(resp => resp.json())
     .then((youbike) => {
         output.textContent = ""
-        youbike.forEach((data) => {
+        const keyword = searchBox.value.trim().toLowerCase() // 小寫比對，不過其實不太知道為什麼沒加上就無法搜尋
+        const filterName = keyword
+            ? youbike.filter(data => {
+                const name = data.sna.replace("YouBike2.0_", "").toLowerCase()
+                const address = data.ar.toLowerCase()
+                return name.includes(keyword) || address.includes(keyword)
+            })
+            : youbike
+
+        filterName.forEach((data) => {
             const name = data.sna.replace("YouBike2.0_", "")
             const address = data.ar
             const bikes = data.available_return_bikes
